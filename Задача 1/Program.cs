c8b14f54737d4ba894c679c092df3283e15df239
danil <jardus@bk.ru>
2022-10-30 18:58:37 +0300

    HomeWork_7

---
+++ Задача 1/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9*/
+
+Console.WriteLine("enter m: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("enter n: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"m = {m}, n = {n} ");
+
+double[,] array = new double[m, n];
+
+void CreateArrayDouble(double[,] array)
+{
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      array[i, j] = new Random().NextDouble() * 20 - 10;
+    }
+  }
+}
+
+void WriteArray (double[,] array){
+for (int i = 0; i < m; i++)
+  {
+      for (int j = 0; j < n; j++)
+      {
+        double Number = Math.Round(array[i, j], 1);
+        Console.Write(Number + " ");
+      }
+      Console.WriteLine();
+  }
+}
+CreateArrayDouble(array);
+WriteArray(array);
+Console.WriteLine();
---
+++ Задача 2/Program.cs
@@ -0,0 +1,52 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет*/
+
+Console.WriteLine("Введите количество строк: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int n = int.Parse(Console.ReadLine());
+int[,] array = new int[5, 5];
+
+void CreateArrayint(int[,] array)
+{
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      array[i, j] = new Random().Next(1,5) ;
+    }
+  }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+           Console.Write($"{array[i,j]} ");
+        }
+       Console.WriteLine();
+    } 
+    
+}
+
+CreateArrayint(array);
+PrintArray(array);
+Console.WriteLine();
+
+Console.WriteLine("Введите строку элемента = ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите столбец элемента = ");
+int y = int.Parse(Console.ReadLine());
+
+if(x>=m || y>=n)
+{Console.WriteLine("Такого элемента не существует");}
+else
+{Console.WriteLine($"Число в позиции элемента ({x},{y}) = "+array[x,y]);}
+
